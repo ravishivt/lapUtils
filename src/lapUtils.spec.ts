928f526e2397edd04eb7a82a616dbdaee26bcd6c
Ravi Mehra <javishi@gmail.com>
2018-08-21 16:47:17 -0700

    Added comments.

--- src/lapUtils.spec.ts
+++ src/lapUtils.spec.ts
@@ -1,7 +1,7 @@
 import {
-  getUserLapCompany,
+  getLapCompanyForUser,
   getLapCompanyTlds,
-  getUserSubDomain,
+  getSubDomainForUser,
   User,
   LapRole,
   LapUser,
@@ -10,7 +10,7 @@ import {
 import { getDomain } from 'tldjs';
 
 const generateUser = (email: string, lapRole: LapRole): User => {
-  const subDomain = getUserSubDomain(email);
+  const subDomain = getSubDomainForUser(email);
   return {
     email,
     subDomain,
@@ -78,49 +78,43 @@ describe('getLapCompanyTlds', async () => {
   });
 });
 
-describe('getUserLapCompany', async () => {
-  const onboardingUserEmail = 'e1@ebay.com';
-  const onboardingUsersRootAccountTlds = ['ebay.com'];
+describe('getLapCompanyForUser', async () => {
+  const userEmail = 'e1@ebay.com'; // User requesting to onboard.
+  const rootAccountTLDs = ['ebay.com']; // The TLDs associated to the user's root account.
 
   it('should return null if LAP returns no companies', () => {
     const lapResponse = [];
-    expect(
-      getUserLapCompany(lapResponse, onboardingUserEmail, onboardingUsersRootAccountTlds),
-    ).toBe(null);
+    expect(getLapCompanyForUser(lapResponse, userEmail, rootAccountTLDs)).toBe(null);
   });
 
   it('should return null if LAP returns no companies that user belongs to', () => {
     const lapResponse = [generateLapCompany(0, [generateUser('g1@google.com', 'Admin')])];
-    expect(
-      getUserLapCompany(lapResponse, onboardingUserEmail, onboardingUsersRootAccountTlds),
-    ).toBe(null);
+    expect(getLapCompanyForUser(lapResponse, userEmail, rootAccountTLDs)).toBe(null);
   });
 
-  it(`should return one company as admin if LAP returns one company as admin and company's TLDs are OK`, () => {
-    const lapResponse = [generateLapCompany(0, [generateUser(onboardingUserEmail, 'Admin')])];
-    expect(
-      getUserLapCompany(lapResponse, onboardingUserEmail, onboardingUsersRootAccountTlds),
-    ).toEqual({
+  it(`should return one company as admin if LAP returns one company as admin and company has no foreign TLDs`, () => {
+    const lapResponse = [generateLapCompany(0, [generateUser(userEmail, 'Admin')])];
+    expect(getLapCompanyForUser(lapResponse, userEmail, rootAccountTLDs)).toEqual({
       companyId: 0,
       companyName: 'Company0',
       role: 'Admin',
     });
   });
 
-  it(`should error if LAP returns one company as admin but company has TLDs not in user's root account.`, () => {
+  it(`should error if LAP returns one company as admin but company has foreign TLDs not in user's root account.`, () => {
     const lapResponse = [
       generateLapCompany(0, [
-        generateUser(onboardingUserEmail, 'Admin'),
+        generateUser(userEmail, 'Admin'),
         generateUser('g1@google.com', 'Admin'),
       ]),
     ];
 
     try {
-      getUserLapCompany(lapResponse, onboardingUserEmail, onboardingUsersRootAccountTlds);
+      getLapCompanyForUser(lapResponse, userEmail, rootAccountTLDs);
       throw new Error('Should have failed');
     } catch (error) {
       expect(error.message).toBe(
-        `Error 19-3: User has one LAP company association as ADMIN but that company has TLDs not in the root account's TLDs`,
+        `Error 19-3: User has one LAP company association as ADMIN but that company has foreign TLDs not in the root account's TLDs`,
       );
     }
   });
@@ -128,78 +122,74 @@ describe('getUserLapCompany', async () => {
   it('should return one company as standard if LAP returns one company as standard', () => {
     const lapResponse = [
       generateLapCompany(0, [
-        generateUser(onboardingUserEmail, 'Standard'),
+        generateUser(userEmail, 'Standard'),
         generateUser('e2@ebay.com', 'Admin'),
       ]),
     ];
-    expect(
-      getUserLapCompany(lapResponse, onboardingUserEmail, onboardingUsersRootAccountTlds),
-    ).toEqual({
+    expect(getLapCompanyForUser(lapResponse, userEmail, rootAccountTLDs)).toEqual({
       companyId: 0,
       companyName: 'Company0',
       role: 'Standard',
     });
   });
 
-  it(`should error if LAP returns one company as standard but company has TLDs not in user's root account.`, () => {
+  it(`should error if LAP returns one company as standard but company has foreign TLDs not in user's root account.`, () => {
     const lapResponse = [
       generateLapCompany(0, [
-        generateUser(onboardingUserEmail, 'Standard'),
+        generateUser(userEmail, 'Standard'),
         generateUser('g1@google.com', 'Admin'),
       ]),
     ];
 
     try {
-      getUserLapCompany(lapResponse, onboardingUserEmail, onboardingUsersRootAccountTlds);
+      getLapCompanyForUser(lapResponse, userEmail, rootAccountTLDs);
       throw new Error('Should have failed');
     } catch (error) {
       expect(error.message).toBe(
-        `Error 19-4: User has one LAP company association as STANDARD but that company has TLDs not in the root account's TLDs`,
+        `Error 19-4: User has one LAP company association as STANDARD but that company has foreign TLDs not in the root account's TLDs`,
       );
     }
   });
 
-  it('should return one company as admin if LAP returns multiple companies but only one is admin', () => {
+  it('should return one company as admin if LAP returns multiple companies but only one is admin.  That company has no foreign TLDs.', () => {
     const lapResponse = [
-      generateLapCompany(0, [generateUser(onboardingUserEmail, 'Admin')]),
-      generateLapCompany(1, [generateUser(onboardingUserEmail, 'Standard')]),
+      generateLapCompany(0, [generateUser(userEmail, 'Admin')]),
+      generateLapCompany(1, [generateUser(userEmail, 'Standard')]),
     ];
-    expect(
-      getUserLapCompany(lapResponse, onboardingUserEmail, onboardingUsersRootAccountTlds),
-    ).toEqual({
+    expect(getLapCompanyForUser(lapResponse, userEmail, rootAccountTLDs)).toEqual({
       companyId: 0,
       companyName: 'Company0',
       role: 'Admin',
     });
   });
 
-  it(`should error if LAP returns multiple companies but only one is admin and the one admin company has TLDs not in user's root account.`, () => {
+  it(`should error if LAP returns multiple companies but only one is admin and the one admin company has foregin TLDs not in user's root account.`, () => {
     const lapResponse = [
       generateLapCompany(0, [
-        generateUser(onboardingUserEmail, 'Admin'),
+        generateUser(userEmail, 'Admin'),
         generateUser('g1@google.com', 'Admin'),
       ]),
-      generateLapCompany(1, [generateUser(onboardingUserEmail, 'Standard')]),
+      generateLapCompany(1, [generateUser(userEmail, 'Standard')]),
     ];
 
     try {
-      getUserLapCompany(lapResponse, onboardingUserEmail, onboardingUsersRootAccountTlds);
+      getLapCompanyForUser(lapResponse, userEmail, rootAccountTLDs);
       throw new Error('Should have failed');
     } catch (error) {
       expect(error.message).toBe(
-        `Error 19-3: User has one LAP company association as ADMIN but that company has TLDs not in the root account's TLDs`,
+        `Error 19-3: User has one LAP company association as ADMIN but that company has foreign TLDs not in the root account's TLDs`,
       );
     }
   });
 
   it('should error if LAP returns multiple companies with admin status for more than one', () => {
     const lapResponse = [
-      generateLapCompany(0, [generateUser(onboardingUserEmail, 'Admin')]),
-      generateLapCompany(1, [generateUser(onboardingUserEmail, 'Admin')]),
+      generateLapCompany(0, [generateUser(userEmail, 'Admin')]),
+      generateLapCompany(1, [generateUser(userEmail, 'Admin')]),
     ];
 
     try {
-      getUserLapCompany(lapResponse, onboardingUserEmail, onboardingUsersRootAccountTlds);
+      getLapCompanyForUser(lapResponse, userEmail, rootAccountTLDs);
       throw new Error('Should have failed');
     } catch (error) {
       expect(error.message).toBe('Error 19-1: User has multiple LAP company associations as ADMIN');
@@ -209,17 +199,17 @@ describe('getUserLapCompany', async () => {
   it('should error if LAP returns multiple companies and user is not an admin for any', () => {
     const lapResponse = [
       generateLapCompany(0, [
-        generateUser(onboardingUserEmail, 'Standard'),
+        generateUser(userEmail, 'Standard'),
         generateUser('e2@ebay.com', 'Admin'),
       ]),
       generateLapCompany(1, [
-        generateUser(onboardingUserEmail, 'Standard'),
+        generateUser(userEmail, 'Standard'),
         generateUser('e2@ebay.com', 'Admin'),
       ]),
     ];
 
     try {
-      getUserLapCompany(lapResponse, onboardingUserEmail, onboardingUsersRootAccountTlds);
+      getLapCompanyForUser(lapResponse, userEmail, rootAccountTLDs);
       throw new Error('Should have failed');
     } catch (error) {
       expect(error.message).toBe(
@@ -228,37 +218,38 @@ describe('getUserLapCompany', async () => {
     }
   });
 
-  it(`should return one company if LAP returns multiple companies and user is not an admin for any but only one company matches the root acccount's TLDs`, () => {
+  it(`should return one company if LAP returns multiple companies and user is not an admin for any but only one company has no foreign TLDs`, () => {
     const lapResponse = [
       generateLapCompany(0, [
-        generateUser(onboardingUserEmail, 'Standard'),
+        generateUser(userEmail, 'Standard'),
         generateUser('e2@ebay.com', 'Admin'),
       ]),
       generateLapCompany(1, [
-        generateUser(onboardingUserEmail, 'Standard'),
+        generateUser(userEmail, 'Standard'),
         generateUser('g1@google.com', 'Admin'),
       ]),
     ];
 
-    expect(
-      getUserLapCompany(lapResponse, onboardingUserEmail, onboardingUsersRootAccountTlds),
-    ).toEqual({
+    expect(getLapCompanyForUser(lapResponse, userEmail, rootAccountTLDs)).toEqual({
       companyId: 0,
       companyName: 'Company0',
       role: 'Standard',
     });
   });
-  it(`should still return one company if LAP returns a company that has multiple TLDs but all exist in the root account's TLDs`, () => {
+
+  // TODO: Scenario 11 from https://gitlab.com/hpe-aruba-get/asp-backend/issues/372 when business decides what to do.
+
+  it(`should return one company if LAP returns one company that has multiple TLDs but all exist in the root account's TLDs`, () => {
     const lapResponse = [
       generateLapCompany(0, [
-        generateUser(onboardingUserEmail, 'Admin'),
+        generateUser(userEmail, 'Admin'),
         generateUser('p1@paypal.com', 'Admin'),
       ]),
     ];
 
     expect(
       // Note that 'abc.com' doesn't exist in the LAP company and it doesn't cause any violation errors.
-      getUserLapCompany(lapResponse, onboardingUserEmail, ['ebay.com', 'paypal.com', 'abc.com']),
+      getLapCompanyForUser(lapResponse, userEmail, ['ebay.com', 'paypal.com', 'abc.com']),
     ).toEqual({
       companyId: 0,
       companyName: 'Company0',
--- src/lapUtils.ts
+++ src/lapUtils.ts
@@ -22,10 +22,10 @@ export interface LapUserCompanyAssociation {
   role: LapRole;
 }
 
-export const getUserSubDomain = (email: string): string =>
+export const getSubDomainForUser = (email: string): string =>
   email.substring(email.lastIndexOf('@') + 1);
 
-const doesCompanyMatchRATlds = (company: LapCompany, tlds: Array<string>) => {
+const doAllCompanyTLDsExistInRA = (company: LapCompany, tlds: Array<string>) => {
   const companyTlds = getLapCompanyTlds(company);
   return companyTlds.length > 0 && companyTlds.every(tld => tlds.indexOf(tld) !== -1);
 };
@@ -34,7 +34,7 @@ export const getLapCompanyTlds = (lapCompany: LapCompany): Array<string> => {
   const tlds = [
     ...lapCompany.UsersList.reduce((adminTlds: Set<string>, lapUser) => {
       if (lapUser.Role === 'Admin') {
-        adminTlds.add(getDomain(getUserSubDomain(lapUser.Email)));
+        adminTlds.add(getDomain(getSubDomainForUser(lapUser.Email)));
       }
       return adminTlds;
     }, new Set()),
@@ -49,7 +49,7 @@ export const getLapCompanyTlds = (lapCompany: LapCompany): Array<string> => {
   return tlds;
 };
 
-export const getUserLapCompany = (
+export const getLapCompanyForUser = (
   lapApiResponse: Array<LapCompany>,
   userEmail: string,
   rootAccountTlds: Array<string>,
@@ -57,7 +57,7 @@ export const getUserLapCompany = (
   const companyHash: { [key: number]: LapCompany } = {};
   // Iterate over list of companies and transform it into a list of user-company associations.
   //   Array.reduce + Array.some allows us to "map" and "filter" array in one  iteration instead of two.
-  const userCompanies = lapApiResponse.reduce(
+  const companies = lapApiResponse.reduce(
     (result: Array<LapUserCompanyAssociation>, lapCompany) => {
       // some - Iterate over company's user list until user is found.
       // Disregard any user-company associations that aren't standard or admin (e.g. reseller).
@@ -74,55 +74,47 @@ export const getUserLapCompany = (
           });
           return true;
         }
-        return false;
       });
       return result;
     },
     [],
   );
-  if (userCompanies.length > 0) {
-    const userAdminCompanies = userCompanies.filter(company => company.role === 'Admin');
+  if (companies.length > 0) {
+    const adminCompanies = companies.filter(company => company.role === 'Admin');
     // Companies that user is an ADMIN to take precedence over other associations.
-    if (userAdminCompanies.length > 0) {
-      if (userAdminCompanies.length === 1) {
-        if (doesCompanyMatchRATlds(companyHash[userAdminCompanies[0].companyId], rootAccountTlds)) {
-          return userAdminCompanies[0];
+    if (adminCompanies.length > 0) {
+      if (adminCompanies.length === 1) {
+        // Make sure the single ADMIN company has no foreign TLDs.
+        if (doAllCompanyTLDsExistInRA(companyHash[adminCompanies[0].companyId], rootAccountTlds)) {
+          return adminCompanies[0];
         } else {
           throw new Error(
-            `Error 19-3: User has one LAP company association as ADMIN but that company has TLDs not in the root account's TLDs`,
+            `Error 19-3: User has one LAP company association as ADMIN but that company has foreign TLDs not in the root account's TLDs`,
           );
         }
-      } else if (userAdminCompanies.length > 1) {
+      } else if (adminCompanies.length > 1) {
         throw new Error('Error 19-1: User has multiple LAP company associations as ADMIN');
       }
     }
     // If no ADMIN associations, process the STANDARD associations.
     else {
-      if (userCompanies.length === 1) {
-        if (doesCompanyMatchRATlds(companyHash[userCompanies[0].companyId], rootAccountTlds)) {
-          return userCompanies[0];
+      if (companies.length === 1) {
+        // Make sure the single STANDARD company has no foreign TLDs.
+        if (doAllCompanyTLDsExistInRA(companyHash[companies[0].companyId], rootAccountTlds)) {
+          return companies[0];
         } else {
           throw new Error(
-            `Error 19-4: User has one LAP company association as STANDARD but that company has TLDs not in the root account's TLDs`,
+            `Error 19-4: User has one LAP company association as STANDARD but that company has foreign TLDs not in the root account's TLDs`,
           );
         }
       }
-      if (userCompanies.length > 0) {
-        //
-        // const userCompaniesMatchingUserTlds = userCompanies.reduce(
-        //   (companies: Array<LapUserCompanyAssociation>, company) => {
-        //     if (doesCompanyMatchRATlds(companyHash[company.companyId], rootAccountTlds)) {
-        //       companies.push(company);
-        //     }
-        //     return companies;
-        //   },
-        //   [],
-        // );
-        const userCompaniesMatchingUserTlds = userCompanies.filter(company =>
-          doesCompanyMatchRATlds(companyHash[company.companyId], rootAccountTlds),
+      if (companies.length > 0) {
+        // Attempt to find a single company where there are no foreign TLDs.  If multiple match, error.
+        const companiesWithAllTLDsInRA = companies.filter(company =>
+          doAllCompanyTLDsExistInRA(companyHash[company.companyId], rootAccountTlds),
         );
-        if (userCompaniesMatchingUserTlds.length === 1) {
-          return userCompaniesMatchingUserTlds[0];
+        if (companiesWithAllTLDsInRA.length === 1) {
+          return companiesWithAllTLDsInRA[0];
         } else {
           throw new Error('Error 19-2: User has multiple LAP company associations as STANDARD');
         }
